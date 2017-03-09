1edc84f53edc0b59f8ab32385ca5c4a8cbce80ac
Sabrina Miller <sabrina@sabris-mac.local>
2017-03-09 01:56:00 -0300

    lets try djangorestframework

---
+++ timeline/events/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from timeline.events.models import Event
+from timeline.users.models import User
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('name')
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('create_date', 'event_type', 'user')
+
